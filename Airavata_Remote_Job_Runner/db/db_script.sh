8ffaebf117b7703f90dd0fd88e94e749a0910327
Pratik Patel <pratik.patel.sh@hotmail.com>
2016-04-10 21:57:44 -0400

    DB_Script

---
+++ Airavata_Remote_Job_Runner/db/db_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+service postgresql status
+if [ "$?" -gt "0" ]
+ then
+        sudo apt-get -y update
+        sudo apt-get -y install postgresql-client postgresql-contrib
+        sudo -u postgres psql postgres -a -f /opt/scripts/airavata_ddl_20160321.sql
+        sudo -u postgres psql postgres -a -f /opt/scripts/airavata_dml_20160321.sq
+ fi
+
